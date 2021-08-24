d07f5e54f352aa56026e440bd816610b255011d7
jefloreste <71605758+jefloreste@users.noreply.github.com>
2021-08-23 21:48:43 -0500

    Update exercise.py

--- assignments/10Videojuego/src/exercise.py
+++ assignments/10Videojuego/src/exercise.py
@@ -1,8 +1,8 @@
-def main():
-    #escribe tu código abajo de esta línea
-   pass
+def main (new, used):
+    return (int)((new*1000)+(used*350))
 
 
+new = int(input("How many new games are you buying: "))
+used = int(input("How many used games are you buying: "))
 
-if __name__ == '__main__':
-    main()
+print("Your total price is:", main (new, used))
